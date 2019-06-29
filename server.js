ec554844ce226e81cb30cef49a7624a54565cc4e
huntsimon23 <hunt.simon23@gmail.com>
2019-06-28 23:11:25 -0400

    server updates and html

--- server.js
+++ server.js
@@ -24,11 +24,10 @@ var cheerio = require("cheerio");
 var axios = require("axios");
 
 var PORT = 3000
+var fox35Url = "http://www.fox35orlando.com"
 // Make a request via axios to grab the HTML body from the site of your choice
 axios.get("http://www.fox35orlando.com/news").then(function(response) {
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
   var $ = cheerio.load(response.data);
 
   // An empty array to save the data that we'll scrape
@@ -45,15 +44,16 @@ axios.get("http://www.fox35orlando.com/news").then(function(response) {
     // Save these results in an object that we'll push into the results array we defined earlier
     results.push({
       title: title,
-      link: link
+      link: fox35Url + link,
     });
   });
 
   // Log the results once you've looped through each of the elements found with cheerio
   console.log(results);
+  console.log("Scrape completed")
 });
 
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+    console.log("App running on https://localhost:" + PORT);
   });
   
