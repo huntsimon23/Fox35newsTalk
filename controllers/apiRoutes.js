30ccf35a22962109a1b31fb5f3ec9e608c078bb2
huntsimon23 <hunt.simon23@gmail.com>
2019-06-29 23:54:27 -0400

    handlebars layout and MVC sructure

---
+++ controllers/apiRoutes.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router();
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("../models");
+
+router.get("/", function(req, res) {
+
+var fox35Url = "http://www.fox35orlando.com";
+
+axios.get("http://www.fox35orlando.com/news").then(function(response) {
+  var $ = cheerio.load(response.data);
+
+  $(".story").each(function(i, element) {
+    var result = {};
+    result.title = $(element).children().text().trim();
+    result.link = fox35Url + $(element).find("a").attr("href");
+
+    db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log("New Article! " + dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+    console.log("Scrape completed!")
+    res.render("index", hbsObject);
+    })
+});  
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,11 +4,9 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
-// var db = require("./models");
+var PORT = 3000
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
 mongoose.connect(MONGODB_URI);
 
 // Initialize Express
@@ -20,38 +18,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
-var cheerio = require("cheerio");
-var axios = require("axios");
 
-var PORT = 3000
-var fox35Url = "http://www.fox35orlando.com"
-// Make a request via axios to grab the HTML body from the site of your choice
-axios.get("http://www.fox35orlando.com/news").then(function(response) {
 
-  var $ = cheerio.load(response.data);
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-  // An empty array to save the data that we'll scrape
-  var results = [];
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $(".story").each(function(i, element) {
+// Import routes and give the server access to them.
+var routes = require("./controllers/apiRoutes.js");
+app.use(routes);
 
-    var title = $(element).children().text().trim();
-    var link = $(element).find("a").attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: fox35Url + link,
-    });
-  });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-  console.log("Scrape completed")
-});
 
 app.listen(PORT, function() {
     console.log("App running on https://localhost:" + PORT);
