661177c2b00f100554c866ec6a54b74d2569f888
huntsimon23 <hunt.simon23@gmail.com>
2019-07-01 18:10:59 -0400

    server update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models/index.js");
 
-var PORT = 3000
+var PORT = process.env.PORT || 3000;
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
